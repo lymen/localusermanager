3dc28752030a9e3fcdc80d0ebeb5bcca2eff478e
Lymen <rulel@codev.com>
2020-10-08 15:51:17 +0800

    Fix static file errors

--- localusermanager/settings.py
+++ localusermanager/settings.py
@@ -125,7 +125,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static/',
-# ]
+# STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
